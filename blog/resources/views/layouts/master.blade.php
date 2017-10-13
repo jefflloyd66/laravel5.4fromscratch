902313bdde3ddd2805dc8b7bb2438089739a1359
Jeff <jeff@home>
2017-10-13 16:10:22 +0100

    lesson 11

--- blog/resources/views/layouts/master.blade.php
+++ blog/resources/views/layouts/master.blade.php
@@ -5,43 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="/favicon.ico">
 
-    <title>Album example for Bootstrap</title>
+    <title>Blog Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="css/album.css" rel="stylesheet">
+    <link href="/css/blog.css" rel="stylesheet">
 </head>
 
 <body>
 
+
 @include('layouts.nav')
 
+@include('layouts.banner')
+
+
+
 <div class="container">
+    <div class="row">
+        <div class="col-sm-8 blog-main">
 
-    @yield('content')
+            @yield('content')
 
-</div>
+        </div>
+
+        @include('layouts.sidebar')
+
+
+    </div><!-- /.row -->
+</div><!-- /.container -->
+
 
 @include('layouts.footer')
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="js/dist/vendor/jquery.min.dist"><\/script>')</script>
-<script src="js/vendor/popper.min.js"></script>
-<script src="js/vendor/holder.min.js"></script>
-<script>
-    $(function () {
-        Holder.addTheme("thumb", { background: "#55595c", foreground: "#eceeef", text: "Thumbnail" });
-    });
-</script>
-<script src="js/dist/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="js/ie10-viewport-bug-workaround.js"></script>
-</body>
-</html>
+
