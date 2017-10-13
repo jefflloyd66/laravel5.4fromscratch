8145fe1d3b0d231750e816dd844243cd9e8e8601
Jeff <jeff@home>
2017-10-13 16:58:39 +0100

    lesson 12

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -24,10 +24,11 @@ class PostsController extends Controller
 
     public function store()
     {
-        Post::create([
-            'title' => request('title'),
-            'body' => request('body')
+        $this->validate(\request(), [
+            'title' => 'required',
+            'body' => 'required'
         ]);
+        Post::create(request(['title', 'body']));
 
         return redirect('/');
     }
--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -21,7 +21,11 @@
                 <textarea class="form-control" id="body" name="body"></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Publish</button>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Publish</button>
+            </div>
+
+            @include('layouts.errors')
 
         </form>
     </div>
