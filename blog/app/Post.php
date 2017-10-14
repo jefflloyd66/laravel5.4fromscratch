3366e24a43fd8e506b2f7e49c1b471c5de67cf2e
Jeff <jeff@home>
2017-10-14 17:02:39 +0100

    lesson 18

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -8,6 +8,11 @@ class Post extends Model
 {
     protected $fillable = ['title', 'body'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
